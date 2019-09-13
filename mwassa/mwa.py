02b8ec703f542c096e53581b9b4950c7b3a9d597
steveprabu <steverajprabu@gmail.com>
2019-09-13 13:53:35 +0800

    creates and mwa observer

--- mwassa/mwa.py
+++ mwassa/mwa.py
@@ -1,10 +1,9 @@
 import ephem
 
 def intialiseMWA():
-	global mwa
  	mwa = ephem.Observer()
 	mwa.lon = '116:40:14.93485'
 	mwa.lat = '-26:42:11.94986'
 	mwa.elevation = 377.827 #from sea level
-	return;
+	return mwa;
 
