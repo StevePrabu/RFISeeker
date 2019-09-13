2caeb178fa20c59a601bad578ae7624daf0bccb3
steveprabu <steverajprabu@gmail.com>
2019-09-13 17:22:54 +0800

    contains the skew normal curve fitting script

--- mwassa/curvefitting.py
+++ mwassa/curvefitting.py
@@ -15,4 +15,19 @@ def skew_norm_pdf(x,e=0,w=1,a=0):
 def reqCDF(seedValue):
         return 0.5*(1+special.erf(float(seedValue)/np.sqrt(2)))
 
+def getSeedValue(cdf,seedSigma,x):
+    diffInCDF = abs(cdf - reqCDF(seedSigma))
+    minDiff = min(diffInCDF)
+    position_of_min = int(np.asarray(np.where(diffInCDF == minDiff)))
+    seedValue = x[position_of_min] + (x[2]-x[1])/2.0
+    return seedValue
+
+
+def getFloodfillValue(cdf,floodfillSigma,x):
+    diffInCDF = abs(cdf - reqCDF(floodfillSigma))
+    minDiff = min(diffInCDF)
+    position_of_min = int(np.asarray(np.where(diffInCDF == minDiff)))
+    floodfillValue = x[position_of_min] + (x[2]-x[1])/2.0
+    return floodfillValue
+
 
