900241e6e81845d023519e13e55cd9596323be3b
steveprabu <steverajprabu@gmail.com>
2019-10-21 13:55:48 +0800

    a;ldskfjadlfkj

--- mwassa/adaptiveMasking.py
+++ mwassa/adaptiveMasking.py
@@ -2,33 +2,39 @@ from astropy.io import fits
 import numpy as np
 
 
-def mask(psfHDU,pointsInit,pointsValue,imgSize):
+def mask(pointsInit,pointsValue,imgSize):
     masked = np.zeros((imgSize,imgSize))
-    psfData = psfHDU[0].data[0,0,:,:]
-    sidelobeSeed = np.std(psfData)*1
-    maskArray = np.asarray(np.where(psfData > 0)).T
-    maskArray[:,0] -= int(imgSize/2.0)
-    maskArray[:,1] -= int(imgSize/2.0)
+    #psfData = psfHDU[0].data[0,0,:,:]
+    #sidelobeSeed = np.std(psfData)*1
+    #maskArray = np.asarray(np.where(psfData > 0)).T
+    #maskArray[:,0] -= int(imgSize/2.0)
+    #maskArray[:,1] -= int(imgSize/2.0)
     outputPoints = []
     #pointSorted = np.asarray([x for _,x in sorted(zip(pointsValue,pointsInit),reverse=True)])
     index = np.argsort(pointsValue)
     pointSorted = pointsInit[np.flip(index)]
     counter = 0
     for currentPoint in pointSorted:
-        if masked[currentPoint[0],currentPoint[1]] ==1:
-            continue
-        else:
-	    if counter  == 0:
-            	outputPoints.append(np.asarray(currentPoint))
-		counter += 1
-            mask = maskArray
-            mask[:,1] += currentPoint[0]
-            mask[:,0] += currentPoint[1]
-            for m in mask:
-                if 0 <= m[0] < imgSize and 0 <= m[1] < imgSize:
-                    masked[m[0],m[1]]=1
-            mask[:,1] -= currentPoint[0]
-            mask[:,0] -= currentPoint[1]
+	if counter == 0:
+	    outputPoints.append(np.asarray(currentPoint))
+	    break
+ 	counter +=1
+    #counter = 0
+    #for currentPoint in pointSorted:
+    #    if masked[currentPoint[0],currentPoint[1]] ==1:
+    #        continue
+    #    else:
+    #	    if counter  == 0:
+    #        	outputPoints.append(np.asarray(currentPoint))
+    #		counter += 1
+    #        mask = maskArray
+    #        mask[:,1] += currentPoint[0]
+    #        mask[:,0] += currentPoint[1]
+    #        for m in mask:
+    #            if 0 <= m[0] < imgSize and 0 <= m[1] < imgSize:
+    #                masked[m[0],m[1]]=1
+    #        mask[:,1] -= currentPoint[0]
+    #        mask[:,0] -= currentPoint[1]
     return outputPoints
 
 
