89bffdb43d56546e4e62b6095b5bbb9115eaca28
steveprabu <steverajprabu@gmail.com>
2019-10-21 09:55:29 +0800

    now finds only the brightest sigma seed from a diff image

--- mwassa/adaptiveMasking.py
+++ mwassa/adaptiveMasking.py
@@ -5,17 +5,22 @@ import numpy as np
 def mask(psfHDU,pointsInit,pointsValue,imgSize):
     masked = np.zeros((imgSize,imgSize))
     psfData = psfHDU[0].data[0,0,:,:]
-    sidelobeSeed = np.std(psfData)*3
-    maskArray = np.asarray(np.where(psfData > sidelobeSeed)).T
+    sidelobeSeed = np.std(psfData)*1
+    maskArray = np.asarray(np.where(psfData > 0)).T
     maskArray[:,0] -= int(imgSize/2.0)
     maskArray[:,1] -= int(imgSize/2.0)
     outputPoints = []
-    pointSorted = np.asarray([x for _,x in sorted(zip(pointsValue,pointsInit),reverse=True)])
+    #pointSorted = np.asarray([x for _,x in sorted(zip(pointsValue,pointsInit),reverse=True)])
+    index = np.argsort(pointsValue)
+    pointSorted = pointsInit[np.flip(index)]
+    counter = 0
     for currentPoint in pointSorted:
         if masked[currentPoint[0],currentPoint[1]] ==1:
             continue
         else:
-            outputPoints.append(np.asarray(currentPoint))
+	    if counter  == 0:
+            	outputPoints.append(np.asarray(currentPoint))
+		counter += 1
             mask = maskArray
             mask[:,1] += currentPoint[0]
             mask[:,0] += currentPoint[1]
